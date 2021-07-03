0427e05dc83ad1358c528193f4f7c10c49ca2d42
kaede_momiji <44082622+kaedefuto@users.noreply.github.com>
2021-07-03 22:20:40 +0900

    0703

---
+++ Todoアプリ/todo/todo/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+
+class TodoModel(models.Model):
+    class Meta:
+        verbose_name = "Todoリスト"
+        verbose_name_plural = 'Todoリスト'
+
+    title = models.CharField(
+        verbose_name="タイトル",
+        max_length=50,
+    )
+    content = models.TextField(
+        verbose_name="内容"
+    )
+    deadline = models.DateTimeField(
+        verbose_name="期日",
+        default=timezone.now
+    )
+
+    def __str__(self):
+        return self.title
---
+++ Todoアプリ/todo/todo/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import TodoModel
+from django.urls import reverse_lazy
+
+# Create your views here.
+class TodoList(generic.ListView):
+    template_name = 'todo/list.html'
+    model = TodoModel
+
+
+class TodoDetail(generic.DetailView):
+    template_name = 'todo/detail.html'
+    model = TodoModel
+
+
+class TodoUpdate(generic.UpdateView):
+    template_name = 'todo/update.html'
+    model = TodoModel
+    fields = ('title', 'content', 'deadline')
+    success_url = reverse_lazy('list')
+
+
+class TodoDelete(generic.DeleteView):
+    template_name = 'todo/delete.html'
+    model = TodoModel
+
+    success_url = reverse_lazy('list')
+
+
+class TodoCreate(generic.CreateView):
+    template_name = 'todo/create.html'
+    model = TodoModel
+    fields = ('title', 'content', 'deadline')
+
+    success_url = reverse_lazy('list')
